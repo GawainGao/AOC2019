4a82eaaaff00b620e672e9f5f928c16eb85d12d2
Gao <archergaoy@gmail.com>
2019-12-04 00:12:37 +0900

    day3picture

--- Day03/day3.py
+++ Day03/day3.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -7,23 +9,31 @@ class Point:
         return Point(self.x + directrion[d][0],self.y+directrion[d][1])
 
 def wireList(li):
+    xaixs = []
+    yaixs = []
     res = {}
     flag = 0
     point = Point(50000,50000)
+    xaixs.append(point.x)
+    yaixs.append(point.y)
     for i in range(len(li)):
         for j in range(int(li[i][1:])):
             flag += 1
             point = point.moveTo(li[i][0])
+            xaixs.append(point.x)
+            yaixs.append(point.y)
             if (point.x,point.y) not in res:
                 res[(point.x,point.y)] = flag
-    return(res)
+    return(res, xaixs, yaixs)
 if __name__ == '__main__':
     with open('day3.txt') as f:
         A, B = [x.split(',') for x in f.readlines()]
 
-    a = wireList(A)
-    b = wireList(B)
-
+    a,ax,ay = wireList(A)
+    b,bx,by = wireList(B)
+    plt.plot(ax,ay)
+    plt.plot(bx,by)
+    plt.show()
     cross = a.keys() & b.keys()
     print(cross)
 
